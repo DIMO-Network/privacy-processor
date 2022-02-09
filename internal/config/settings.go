9798e1870b87cd37bd5472a3087b51ed884be0ad
Dylan Moreland <dylan@dimo.zone>
2022-02-09 15:02:55 -0500

    Sample settings file

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -3,8 +3,8 @@ package config
 // Settings contains the application config
 type Settings struct {
 	KafkaBrokers                  string `yaml:"KAFKA_BROKERS"`
-	DeviceStatusTopic             string `yaml:"DEVICE_STATUS_TOPIC"`
-	DeviceStatusPrivateTopic      string `yaml:"DEVICE_STATUS_PRIVATE_TOPIC"`
-	PrivacyFenceTopic             string `yaml:"PRIVACY_FENCE_TOPIC"`
 	PrivacyProcessorConsumerGroup string `yaml:"PRIVACY_PROCESSOR_CONSUMER_GROUP"`
+	DeviceStatusTopic             string `yaml:"DEVICE_STATUS_TOPIC"`
+	PrivacyFenceTopic             string `yaml:"PRIVACY_FENCE_TOPIC"`
+	DeviceStatusPrivateTopic      string `yaml:"DEVICE_STATUS_PRIVATE_TOPIC"`
 }
