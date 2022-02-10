83a13f9811a41ec5bb0e71c1d987581f182ee14f
Dylan Moreland <dylan@dimo.zone>
2022-02-10 18:10:36 -0500

    Split out test parts

--- internal/processors/privacy_test.go
+++ internal/processors/privacy_test.go
@@ -11,7 +11,7 @@ import (
 
 func ref(x float64) *float64 { return &x }
 
-func TestMain(t *testing.T) {
+func TestPrivacy(t *testing.T) {
 	gt := tester.New(t)
 	log := zerolog.Nop()
 
@@ -37,25 +37,52 @@ func TestMain(t *testing.T) {
 		H3Indexes: []string{"872ab259affffff", "872ab259effffff"},
 	}})
 
-	gt.Consume(string(fg.StatusInput), deviceID, &StatusEvent{Data: StatusData{
-		Latitude:  ref(42.26172693660968),
-		Longitude: ref(-83.71029708818693),
-		Overflow:  map[string]interface{}{},
-	}})
+	t.Run("WithinFence", func(t *testing.T) {
+		gt.Consume(string(fg.StatusInput), deviceID, &StatusEvent{Data: StatusData{
+			Latitude:  ref(42.26172693660968),
+			Longitude: ref(-83.71029708818693),
+			Overflow:  map[string]interface{}{},
+		}})
 
-	key, value, valid := out.Next()
-	if !valid {
-		t.Error("No output")
-	}
-	if key != deviceID {
-		t.Errorf("Expected output to maintain the device ID %s as the key, but got %s", deviceID, key)
-	}
+		key, value, valid := out.Next()
+		if !valid {
+			t.Error("No output")
+		}
+		if key != deviceID {
+			t.Errorf("Expected output to maintain the device ID %s as the key, but got %s", deviceID, key)
+		}
 
-	event := value.(*StatusEvent)
-	if *event.Data.Latitude != 42.25362819577089 || *event.Data.Longitude != -83.68562802176137 {
-		t.Errorf("Expected %f, %f in the output but got %f, %f",
-			42.25362819577089, -83.68562802176137,
-			*event.Data.Latitude, *event.Data.Longitude,
-		)
-	}
+		event := value.(*StatusEvent)
+		if *event.Data.Latitude != 42.25362819577089 || *event.Data.Longitude != -83.68562802176137 {
+			t.Errorf("Expected %f, %f in the output but got %f, %f",
+				42.25362819577089, -83.68562802176137,
+				*event.Data.Latitude, *event.Data.Longitude,
+			)
+		}
+	})
+
+	t.Run("OutsideFence", func(t *testing.T) {
+		gt.Consume(string(fg.StatusInput), deviceID, &StatusEvent{Data: StatusData{
+			Latitude:  ref(42.261123478313145),
+			Longitude: ref(-83.68613574673722),
+			Overflow:  map[string]interface{}{},
+		}})
+
+		key, value, valid := out.Next()
+		if !valid {
+			t.Error("No output")
+		}
+		if key != deviceID {
+			t.Errorf("Expected output to maintain the device ID %s as the key, but got %s", deviceID, key)
+		}
+
+		event := value.(*StatusEvent)
+		if *event.Data.Latitude != 42.261123478313145 || *event.Data.Longitude != -83.68613574673722 {
+			t.Errorf("Expected %f, %f in the output but got %f, %f",
+				42.261123478313145, -83.68613574673722,
+				*event.Data.Latitude, *event.Data.Longitude,
+			)
+		}
+	})
+
 }
